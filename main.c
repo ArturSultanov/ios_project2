09d48eca826870ee22ae593aaf235499b5aa2397
Artur Sultanov <sultanov.personal@gmail.com>
2023-04-21 18:09:59 +0200

    update

--- main.c
+++ main.c
@@ -2,12 +2,28 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 int main(int argc, char* argv[]){
     int id = fork();
-    printf("Hello World, parent\n");
+    int n;
     if (id == 0){
-        printf("Hello World, child\n");
+        n = 1;
     }
+    else{
+        n = 6;
+    }
+
+    if (id != 0){
+        wait(NULL);
+    }
+    
+    int i;
+    for (i = n; i < n + 5; i++)
+    {
+        printf ("%d ", i); 
+        fflush (stdout);
+    }
+
     return 0;
 }
