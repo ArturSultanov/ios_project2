0c33d30840d5c17fb9580f7e5bd0a12af2f8bc67
Artur Sultanov <sultanov.personal@gmail.com>
2023-04-29 20:28:52 +0200

    update

--- main.h
+++
@@ -1,8 +0,0 @@
-typedef struct {
-    int NZ; //number of clients
-    int NU; //number of workers
-    int TZ; //Time in ms where client waiting to go for post
-    int TU; //worker rest, in ms
-    int F; //in ms, time after that post office will be closed  
-}data_t;
-typedef enum {OPEN, CLOSED} PostStatus;
--- tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-    // sem_customer_service2 = sem_open(SEMAPHORE_CUSTOMER_SERVICE2, O_CREAT | O_EXCL, 0666, 0) ;
-    // if (sem_customer_service2 == SEM_FAILED){
-    //     return 1;
-    // }
