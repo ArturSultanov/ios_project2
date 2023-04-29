33896c02abcde68152d6d56d16b699125c8e3685
Artur Sultanov <sultanov.personal@gmail.com>
2023-04-29 20:39:17 +0200

    update

--- proj2.c
+++ proj2.c
@@ -1,53 +1,57 @@
 // Artur Sultanov
 // xsulta01
 // IOS project 2 2023
+#include "proj2_header.h"
+// #include <stdio.h>
+// #include <stdbool.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <time.h>
+// #include <sys/types.h>
+// #include <sys/ipc.h>
+// #include <sys/sem.h>
+// //#include <sys/shm.h>
+// //#include <errno.h>
+// #include <fcntl.h>
+// #include <semaphore.h>
+// #include <sys/stat.h>
+// #include <sys/wait.h>
+// #include <sys/mman.h>
+// #include <signal.h>
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-//#include <sys/shm.h>
-//#include <errno.h>
-#include <fcntl.h>
-#include <semaphore.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <sys/mman.h>
-
-#include <signal.h>
-
-
-
-#define SEMAPHORE_MUTEX "/xsulta01_sem_mutex"
-#define SEMAPHORE_SERVICEFRST "/xsulta01_servicefrst" 
-#define SEMAPHORE_SERVICESCND "/xsulta01_servicescnd" 
-#define SEMAPHORE_SERVICETHRD "/xsulta01_servicethrd" 
-#define SEMAPHORE_CLERK "/xsulta01_clerk"
-#define SEMAPHORE_CUSTOMER "/xsulta01_customer"
+// #define SEMAPHORE_MUTEX "/xsulta01_sem_mutex"
+// #define SEMAPHORE_SERVICEFRST "/xsulta01_servicefrst" 
+// #define SEMAPHORE_SERVICESCND "/xsulta01_servicescnd" 
+// #define SEMAPHORE_SERVICETHRD "/xsulta01_servicethrd" 
+// #define SEMAPHORE_CLERK "/xsulta01_clerk"
+// #define SEMAPHORE_CUSTOMER "/xsulta01_customer"
 
 
-#define NUM_SERVICES 3
-#define upsleep_for_random_time(time_max) usleep((rand() % (time_max + 1)) * 1000)
+// #define NUM_SERVICES 3
+// #define upsleep_for_random_time(time_max) usleep((rand() % (time_max + 1)) * 1000)
 
 
-// Function prototypes
-void semaphore_dest(void);
-int semaphore_init(void);
-int shared_memory_dest(void);
-int shared_memory_init(void);
-void customer_process(int idZ, int TZ);
-void clerk_process(int idU, int TU);
-void cleanup(void);
-void kill_child_processes(void);
+// // Function prototypes
+// void semaphore_dest(void);
+// int semaphore_init(void);
+// int shared_memory_dest(void);
+// int shared_memory_init(void);
+// void customer_process(int idZ, int TZ);
+// void clerk_process(int idU, int TU);
+// void cleanup(void);
+// void kill_child_processes(void);
 
 // Global values
 FILE *file;
 pid_t *child_processes;
 int child_count;
+
+sem_t *sem_mutex;
+sem_t *sem_first_service;
+sem_t *sem_second_service;
+sem_t *sem_third_service;
+sem_t *sem_clerk;
+sem_t *sem_customer;
 
 // Shared memory declaration
 bool *post_is_closed = NULL;
@@ -59,14 +63,7 @@ int *clerks_number = NULL;
 int *customers_numbers = NULL;
 
 
-// deklaracia semaforov
-sem_t *sem_mutex;
-sem_t *sem_first_service;
-sem_t *sem_second_service;
-sem_t *sem_third_service;
-sem_t *sem_clerk;
-sem_t *sem_customer;
-
+// d
 
 
 // Kill all Child-processes of Main-process 
--- proj2_header.h
+++ proj2_header.h
@@ -5,8 +5,42 @@
 #ifndef PROJ2_HEADER_FILE
 #define PROJ2_HEADER_FILE
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+//#include <sys/shm.h>
+//#include <errno.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <signal.h>
+
+#define SEMAPHORE_MUTEX "/xsulta01_sem_mutex"
+#define SEMAPHORE_SERVICEFRST "/xsulta01_servicefrst" 
+#define SEMAPHORE_SERVICESCND "/xsulta01_servicescnd" 
+#define SEMAPHORE_SERVICETHRD "/xsulta01_servicethrd" 
+#define SEMAPHORE_CLERK "/xsulta01_clerk"
+#define SEMAPHORE_CUSTOMER "/xsulta01_customer"
 
 
+#define NUM_SERVICES 3
+#define upsleep_for_random_time(time_max) usleep((rand() % (time_max + 1)) * 1000)
 
+// Function prototypes
+void semaphore_dest(void);
+int semaphore_init(void);
+int shared_memory_dest(void);
+int shared_memory_init(void);
+void customer_process(int idZ, int TZ);
+void clerk_process(int idU, int TU);
+void cleanup(void);
+void kill_child_processes(void);
 
 #endif
