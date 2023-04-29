55ef6b1cdf4b866021cf39901b5ba2798d06deab
Artur Sultanov <sultanov.personal@gmail.com>
2023-04-29 21:19:50 +0200

    update

--- proj2.c
+++ proj2.c
@@ -2,44 +2,6 @@
 // xsulta01
 // IOS project 2 2023
 #include "proj2_header.h"
-// #include <stdio.h>
-// #include <stdbool.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <time.h>
-// #include <sys/types.h>
-// #include <sys/ipc.h>
-// #include <sys/sem.h>
-// //#include <sys/shm.h>
-// //#include <errno.h>
-// #include <fcntl.h>
-// #include <semaphore.h>
-// #include <sys/stat.h>
-// #include <sys/wait.h>
-// #include <sys/mman.h>
-// #include <signal.h>
-
-// #define SEMAPHORE_MUTEX "/xsulta01_sem_mutex"
-// #define SEMAPHORE_SERVICEFRST "/xsulta01_servicefrst" 
-// #define SEMAPHORE_SERVICESCND "/xsulta01_servicescnd" 
-// #define SEMAPHORE_SERVICETHRD "/xsulta01_servicethrd" 
-// #define SEMAPHORE_CLERK "/xsulta01_clerk"
-// #define SEMAPHORE_CUSTOMER "/xsulta01_customer"
-
-
-// #define NUM_SERVICES 3
-// #define upsleep_for_random_time(time_max) usleep((rand() % (time_max + 1)) * 1000)
-
-
-// // Function prototypes
-// void semaphore_dest(void);
-// int semaphore_init(void);
-// int shared_memory_dest(void);
-// int shared_memory_init(void);
-// void customer_process(int idZ, int TZ);
-// void clerk_process(int idU, int TU);
-// void cleanup(void);
-// void kill_child_processes(void);
 
 // Global values
 FILE *file;
@@ -61,10 +23,6 @@ int *third_service_queue = NULL;
 int *action_number = NULL;
 int *clerks_number = NULL;
 int *customers_numbers = NULL;
-
-
-// d
-
 
 // Kill all Child-processes of Main-process 
 void kill_child_processes(void) {
